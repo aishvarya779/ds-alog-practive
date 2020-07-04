9c434a21993e4b18b3fb7b27da26b4c8a52b54cb
aishvarya779 <aishvaryatiwari123@gmail.com>
2020-07-04 22:25:22 +0530

    link added

--- code-1.js
+++ code-1.js
@@ -3,6 +3,8 @@ An array is a type of data structure that stores elements of the same type in a 
 In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as 
  (you may also see it written as ).
 Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
+
+link : https://www.hackerrank.com/challenges/arrays-ds/problem
 */
 
 const reverseOrder = arr => {
