684b7df365b5cdd9c532bcc48b7b37ecf78667c9
aishvarya779 <aishvaryatiwari123@gmail.com>
2020-07-09 20:30:15 +0530

    intersection problem

--- linked-list-intersection.js
+++ linked-list-intersection.js
@@ -1,68 +1,53 @@
 // import {Linkedlist from './singly-linked-list';
 const { Linkedlist } = require('./singly-linked-list');
 const list = new Linkedlist();
-list.insertAtHead(3);
-list.insertAtHead(6);
-list.insertAtHead(9);
-list.insertAtHead(15);
 list.insertAtHead(30);
+list.insertAtHead(15);
+list.insertAtHead(9);
+list.insertAtHead(6);
+list.insertAtHead(3);
 // console.log(list);
 const list2 = new Linkedlist();
-list2.insertAtHead(10);
+list2.insertAtHead(30);
 list2.insertAtHead(15);
-list2.insertAtHead(25);
-const interSectionNode = (list1, list2) => {
-  let m = getLinkedListLength(list1.head);
-  let n = getLinkedListLength(list2.head);
-  let greaterList = m > n ? list1.head : list2.head;
-  let smallerList = m > n ? list2.head : list1.head;
-  let interSectionNode;
-  while (!interSectionNode && smallerList.next) {
-    if (checkNodeExist(greaterList, smallerList)) {
-      interSectionNode = smallerList;
-    }
-    smallerList = smallerList.next;
-  }
-  return interSectionNode;
-};
+list2.insertAtHead(10);
+list2.insertAtHead(12);
 
-const checkNodeExist = (list, node) => {
-  let res = false;
-  while (!res && list.next) {
-    if (list.value === node.value) {
-      res = true;
-    }
-    list = list.next;
-  }
+// console.log(interSectionNode(list, list2));
 
-  return res;
-};
+// console.log('Second', listInterSection(list, list2));
+// Y problem new implementation
 const getLinkedListLength = list => {
   let length = 0;
-  while (list.next) {
+  while (list) {
     length++;
     list = list.next;
   }
   return length;
 };
-
-console.log(interSectionNode(list, list2));
-
-// method 2nd for intersection;
-
 const listInterSection = (list1, list2) => {
-  let intersectNode;
-  list1 = list1.head;
-  list2 = list2.head;
   while (list1.next && list2.next) {
     if (list1.value === list2.value) {
-      intersectNode = list1;
+      return list1.value;
     }
     list1 = list1.next;
     list2 = list2.next;
   }
 
-  return intersectNode;
+  return -1;
 };
 
-console.log('Second', listInterSection(list, list2));
+const interSectionMethodThird = (list1, list2) => {
+  let intersectNode;
+  let m = getLinkedListLength(list1.head);
+  let n = getLinkedListLength(list2.head);
+  let greaterList = m > n ? list1.head : list2.head;
+  let smallerList = m < n ? list1.head : list2.head;
+  let difference = Math.abs(m - n);
+  for (let start = 0; start < difference; start++) {
+    greaterList = greaterList.next;
+  }
+  console.log('greaterList', greaterList);
+  return listInterSection(greaterList, smallerList);
+};
+console.log('Result', interSectionMethodThird(list, list2));
